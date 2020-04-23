4c3f95f77180c9453a3a28f8fb056a6e75d478b7
BuildTools <unconfigured@null.spigotmc.org>
2020-04-23 17:12:20 +0900

    changed code a little

--- dom.js
+++ dom.js
@@ -4,9 +4,7 @@ param tag(string): html element
       values(array): child elements  
 */
 
-var Dom = Dom || {};
-
-(function(_this) {
+var Dom = (function() {
   "use strict";
 
   // values가 비어있다거나 아예 인자에 없다는건 추가하거나 변경하고 싶지 않고 이전 상태 그대로 두고 싶다는 의미다.
@@ -31,7 +29,7 @@ var Dom = Dom || {};
     });
   }
 
-  _this.createDom = function(tag, attrs, values) {
+  var create = function(tag, attrs, values) {
     var el = document.createElement(tag);
 
     setAttributes(el, attrs);
@@ -39,7 +37,7 @@ var Dom = Dom || {};
     return el;
   };
 
-  _this.updateDom = function(selector, attrs, values) {
+  var update = function(selector, attrs, values) {
     var targetEl = document.querySelector(selector);
     if (!targetEl) return;
 
@@ -47,33 +45,43 @@ var Dom = Dom || {};
     setValues(targetEl, values);
     return targetEl;
   };
-})(Dom);
+
+  return {
+    create,
+    update
+  };
+})();
 
 window.onload = function() {
   console.log("load completed");
-  var formDiv = Dom.createDom(
-    "div",
-    { class: "container", id: "list-wrapper" },
-    [
-      Dom.createDom("input", {
-        class: "email",
-        id: "email",
-        type: "text",
-        placeholder: "Type your email"
-      }),
-      Dom.createDom("input", {
-        id: "submit",
-        type: "submit",
-        value: "send email",
-        class: "btn"
-      })
-    ]
-  );
-
-  var mainDiv = Dom.createDom("div", { class: "main" }, [
-    Dom.createDom("h1", { class: "title" }, ["main page"]),
-    Dom.createDom("textarea", { class: "text-area" })
+  var formDiv = Dom.create("div", { class: "container", id: "list-wrapper" }, [
+    Dom.create("input", {
+      class: "email",
+      id: "email",
+      type: "text",
+      placeholder: "Type your email"
+    }),
+    Dom.create("input", {
+      id: "submit",
+      type: "submit",
+      value: "send email",
+      class: "btn"
+    })
   ]);
+
+  console.time("build dom with javascript");
+  var mainDiv = Dom.create("div", { class: "main" }, [
+    Dom.create("h1", { class: "title" }, ["main page"]),
+    Dom.create("textarea", { class: "text-area" })
+  ]);
+  console.timeEnd("build dom with javascript");
+
+  // console.time("build dom with html");
+  // var mainDiv = `<div class="main">
+  // <h1 class="title">main page</h1>
+  // <textarea class="text-area"/>
+  // </div>`;
+  // console.timeEnd("build dom with html");
 
   document.getElementById("root").append(formDiv);
 
@@ -84,7 +92,7 @@ window.onload = function() {
 
     // id가 root인 요소객체에 mainDiv 컴포넌트를 삽입힌다.
     // {}, [] 또는 null 자유자재로 설정할 수 있다. 또는 인자없이도 함수가 정상 실행된다.
-    var changedRoot = Dom.updateDom("#root", null, [mainDiv]);
+    var changedRoot = Dom.update("#root", null, [mainDiv]);
     console.log(changedRoot);
   };
 };
